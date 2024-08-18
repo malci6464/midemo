5ec0648254d5c5ebdef25a6e8e4ce43884366c59
Malcolm Isaacs <mis@telenormaritime.com>
2024-08-18 11:44:00 +0200

    diff the build

---
+++ dist/_astro/hoisted.BSkmva3x.js
@@ -0,0 +1,1 @@
+import"./hoisted.Dc_NH-7Y.js";document.addEventListener("astro:page-load",()=>{const t=document.querySelector(".copy-url-button");t?.addEventListener("click",async()=>{await r(t)});async function r(e){let a=e.getAttribute("data-url")||"",n=e.innerText;await navigator.clipboard.writeText(a),e.innerText="Copied",setTimeout(()=>{e.innerText=n},2500)}});
